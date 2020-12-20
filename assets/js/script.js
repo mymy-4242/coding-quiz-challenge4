da37b27f263f38f0a6c852b77ca0c611cc404812
Local MM <73925149+mymy-4242@users.noreply.github.com>
2020-12-20 14:44:58 -0800

    added opener

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,24 @@ var startBtn = document.getElementById('start');
 var submitBtn = document.getElementById('submit');
 var resultsContainer = document.getElementById('results');
 var timerEl = document.getElementById('timer');
+var body = document.body;
 
+function opener() {
+    //title
+    var h1El = document.createElement("h1");
+    h1El.textContent = 'Coding Quiz Challenge'
+    h1El.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+    body.appendChild(h1El);
+
+    //instructions
+    var h2El = document.createElement("h2");
+    h2El.textContent = 'Try to answer the following code-related questions before the time runs out. Keep in mind that wrong answers will result in a penalize your time by 10 seconds!';
+    h2El.setAttribute('style', 'margin:auto; width:100%; text-align:center;');
+    body.appendChild(h2El);
+
+    //on start, begin quiz
+    startBtn.onclick = timer;
+}
 
 function timer() {
     var timeLeft = 60;
@@ -20,17 +37,15 @@ function timer() {
     }, 1000);
 }
 
-//function opener() {
 
-//}
-
-
-//on start, begin quiz
-startBtn.onclick = timer;
-//startBtn.addEventListener('click', quiz);
 
 //display opener
-//opener();
+opener();
+
+
+//startBtn.addEventListener('click', quiz);
+
+
 
 //on submit, go to results
 //submitBtn.addEventListener('click', question);
